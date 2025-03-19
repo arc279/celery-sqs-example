137d5cc6f5b7053e0254ed099aa79cb7906e3cc0
Motonobu Kuryu <revision279@gmail.com>
2025-03-19 13:12:33 +0900

    add: region

--- src/jobs/app.py
+++ src/jobs/app.py
@@ -6,11 +6,16 @@ from celery import Celery
 BROKER_URL = os.getenv("CELERY_BROKER_URL", "sqs://")
 TASK_DEFAULT_QUEUE = os.getenv("CELERY_TASK_DEFAULT_QUEUE", "celery-sqs-example")
 
+SQS_AWS_REGION = os.getenv("AWS_DEFAULT_REGION", "ap-northeast-1")
+
 app = Celery(__name__, broker=BROKER_URL)
 print(f"queue name: {TASK_DEFAULT_QUEUE}")
 
 # cf. https://qiita.com/hankehly/items/c3e0496eb04327a53ac4
 app.conf.update(
+    broker_transport_options={
+        "region": SQS_AWS_REGION,
+    },
     task_serializer="json",
     result_serializer="json",
     accept_content=["json"],
