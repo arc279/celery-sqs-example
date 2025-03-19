45894bdad0bc59ca835a4d83adbf86b4c7a9bd8a
Motonobu Kuryu <revision279@gmail.com>
2025-03-19 10:50:56 +0900

    add: cf.

--- src/jobs/worker.py
+++ src/jobs/worker.py
@@ -1,3 +1,4 @@
+# cf. https://ravinder-chadha.medium.com/multi-process-different-tasks-with-the-same-worker-in-python-using-celery-cee303decf27
 from src.jobs.tasks.task1 import *
 from src.jobs.tasks.task2 import *
 from src.jobs.tasks.task3 import *
